a6215f5db79673c7a0f09f1bbccdc43c18f657be
Jef <jefersonsrodrigues@outlook.com>
2024-11-29 22:09:17 -0300

    29_11_24

--- app.js
+++ app.js
@@ -9,5 +9,26 @@ app.get ('/', (req, res) => {
     res.status(200).send(carros2024);
 });
 
+app.get ('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase(); // Obtendo a sigla //toUpperCase= aceita inf. maiuscula e minuscula
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); // Busca o carro pela sigla
+    if (!carro){
+    // ! vazio Not
+        res.status(404).send("Não exite um carro com a sigla informada!"); //mensagem de erro 
+    return;
+    }
+    res.status(200).send(carro); // resposta afirmativa (OK)
+});
+
+app.post("/", (req, res) => {
+    const novoCarro = req.body; // Obtém o corpo enviado para incluir um carro
+    carros2024.push(novoCarro); // Adiciona o novo carro à lista de carros 
+    res.status(200).send(novoCarro); //Retona o carro adicionado com status 200 (OK)
+
+})
+
+// inicia o servidor da porta 30001:
 app.listen(3001,() => console.log("Servidor Funcionando..."));
 
+// node app.js
+// localhost:30001/
--- tabelacarros.js
+++ tabelacarros.js
@@ -3,6 +3,7 @@
 //const =  Criação de constante
 //let = Criação de variavel 
 //ARRAY 1 
+
 const carros2024 = [
     {
         nome: 'Ferrari',
@@ -14,8 +15,6 @@ const carros2024 = [
         preco: 300000,
     },
 
-// ARRAY 2 = Laço de repetição 
-
     {
         nome: 'Lamborghini',
         sigla: 'LAM',
@@ -25,6 +24,96 @@ const carros2024 = [
         acelaracao: 2.8,
         preco: 330000,
     },
+
+    {
+        nome: 'Ford',
+        sigla: 'HON',
+        velocidadeMaxima: 220,
+        potencia: 200,
+        consumo: 12.0,
+        acelaracao: 6.5,
+        preco: 350000,
+    },
+
+    {
+        nome: 'Fiat',
+        sigla: 'FIA',
+        velocidadeMaxima: 355,
+        potencia: 770,
+        consumo: 6.2,
+        acelaracao: 2.8,
+        preco: 250000,
+    },
+
+    {
+        nome: 'Toyota Corolla',
+        sigla: 'TOY',
+        velocidadeMaxima: 200,
+        potencia: 170,
+        consumo: 15.0,
+        aceleracao: 7.8,
+        preco: 80000,
+    },
+
+    {
+        nome: 'BMW ',
+        sigla: 'BMW',
+        velocidadeMaxima: 250,
+        potencia: 258,
+        consumo: 8.5,
+        aceleracao: 5.8,
+        preco: 180000,
+    },
+
+    {
+        nome: 'Porsche 911',
+        sigla: 'POR',
+        velocidadeMaxima: 315,
+        potencia: 500,
+        consumo: 9.5,
+        aceleracao: 3.7,
+        preco: 280000,
+    },
+
+    {
+        nome: 'Audi A4',
+        sigla: 'AUD',
+        velocidadeMaxima: 250,
+        potencia: 245,
+        consumo: 7.8,
+        aceleracao: 5.9,
+        preco: 170000,
+    },
+
+    {
+        nome: 'Mercedes-Benz C-Class',
+        sigla: 'MBZ',
+        velocidadeMaxima: 250,
+        potencia: 258,
+        consumo: 8.2,
+        aceleracao: 5.7,
+        preco: 190000,
+    },
+
+    {
+        nome: 'Nissan GT-R',
+        sigla: 'NIS',
+        velocidadeMaxima: 315,
+        potencia: 565,
+        consumo: 11.5,
+        aceleracao: 2.8,
+        preco: 250000,
+    },
+
+    {
+        nome: 'Hyundai i30 N',
+        sigla: 'HYU',
+        velocidadeMaxima: 250,
+        potencia: 275,
+        consumo: 9.0,
+        aceleracao: 5.9,
+        preco: 130000,
+    }
 ];
 
 export default carros2024;
